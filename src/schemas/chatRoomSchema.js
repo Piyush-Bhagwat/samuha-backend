5c8a6a70c81cee702e046c3990d1601bc2b175c2
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-10-29 14:17:12 +0530

    changed room schma

--- src/schemas/chatRoomSchema.js
+++ src/schemas/chatRoomSchema.js
@@ -3,7 +3,8 @@ import { Schema, model } from "mongoose";
 const roomSchema = new Schema({
     code: { type: String, unique: true },
     name: String,
-    admin: String,
+    adminID: String,
+    adminName: String,
     messages: [{ type: Schema.Types.ObjectId, ref: "message" }],
     members: [{ type: String }],
     size: Number,
